dae5d8f2c401f0a3b814cbf6d8597f4c3982fe27
souravkumar <s4056751@gmail.com>
2026-08-11 13:24:28 +0530

    feat: added the dto controller and service of analytics service

--- analytics-service/src/main/java/com/souravio/InsightFlow/analytics_service/controller/AnalyticsController.java
+++ analytics-service/src/main/java/com/souravio/InsightFlow/analytics_service/controller/AnalyticsController.java
@@ -1,3 +1,28 @@
 package com.souravio.InsightFlow.analytics_service.controller;
 
-public class AnalyticsController {}
+import com.souravio.InsightFlow.analytics_service.dto.AnalyticsResultResponse;
+import com.souravio.InsightFlow.analytics_service.service.AnalyticsResultService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/analytics")
+@RequiredArgsConstructor
+public class AnalyticsController {
+
+    private final AnalyticsResultService analyticsResultService;
+
+    @GetMapping("/datasets/{datasetId}")
+    public ResponseEntity<AnalyticsResultResponse> getDatasetAnalytics(
+            @PathVariable UUID datasetId
+    ) {
+
+        AnalyticsResultResponse response =
+                analyticsResultService.getAnalytics(datasetId);
+
+        return ResponseEntity.ok(response);
+    }
+}
